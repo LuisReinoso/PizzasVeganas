ca85db38664125622b12430b37d1ae2f5166f5d4
luis <lomejordejr@riseup.net>
2017-08-14 21:15:53 -0500

    Agregado modelo Usuario

---
+++ api/models/Usuario.js
@@ -0,0 +1,28 @@
+/**
+ * Usuario.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+      nombres: {
+        type: 'string',
+        required: true
+      },
+      apellidos: {
+        type: 'string',
+        required: true
+      },
+      correo: {
+        type: 'string',
+        email: true,
+        unique: true
+      },
+      password: {
+        type: 'string'
+      }
+  }
+};
