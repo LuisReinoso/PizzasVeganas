60246f86a1431fa9b49b0c622533f7ebdc3698d8
luis <lomejordejr@riseup.net>
2017-08-14 21:20:45 -0500

    Agregado Pizza

---
+++ api/models/Pizza.js
@@ -0,0 +1,16 @@
+/**
+ * Pizza.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    idUsuario: {
+      model:'Usuario',
+      required: true
+    }
+  }
+};
