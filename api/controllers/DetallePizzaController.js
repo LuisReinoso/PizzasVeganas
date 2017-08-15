6c766eac5cd9c9727b101ac56b18c0560be34f91
luis <lomejordejr@riseup.net>
2017-08-14 21:42:16 -0500

    Agregado controladores DetallePizz y Pizza

---
+++ api/controllers/DetallePizzaController.js
@@ -0,0 +1,10 @@
+/**
+ * DetallePizzaController
+ *
+ * @description :: Server-side logic for managing Ingredientes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
---
+++ api/controllers/PizzaController.js
@@ -0,0 +1,10 @@
+/**
+ * PizzaController
+ *
+ * @description :: Server-side logic for managing Ingredientes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  
+};
